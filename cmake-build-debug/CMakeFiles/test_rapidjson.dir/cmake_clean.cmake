f2c18babb8d5edee9292495dded4cb4284286641
shentongtong <shentongtong@sogou-inc.com>
2019-08-06 16:03:10 +0800

    submit relevant code

---
+++ cmake-build-debug/CMakeFiles/test_rapidjson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_rapidjson.dir/main.cpp.o"
+  "test_rapidjson.pdb"
+  "test_rapidjson"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_rapidjson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
